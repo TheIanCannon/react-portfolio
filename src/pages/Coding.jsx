1e27833a50a5169a09a750e8ff4e6129896e91ef
Ian Cannon <ian.cannon@protonmail.com>
2022-02-22 16:18:10 -0800

    Add navbar to all pages

--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -1,35 +1,37 @@
 import "App" from '../App.css';
+import "Navbar" from '../components/Navbar.jsx';
+import "media" from '../media';
 
 export default function (){
 
 				const projectList = [
 						[
 								"One-Armed Bandit",
-								"../media/img/OneArmedBandit.png",
+								"media/img/OneArmedBandit.png",
 								"http://theiancannon.github.io/GA-Project1-SlotMachine/",
 								"An HTML/CSS/JS-based slot machine with animated *.gif reels and contextual sound effects"
 						],
 						[
 								"Palanthas",
-								"../media/img/Palanthas.png",
+								"media/img/Palanthas.png",
 								"http://project-palanthas.herokuapp.com/",
 								"Developed in MongoDB/Express/Node for collecting, reviewing, and rating books"
 						],
 						[
 								"Ship Collector",
-								"../media/img/ShipCollector.png",
+								"media/img/ShipCollector.png",
 								"http://shipcollector.herokuapp.com/",
 								"Created using Python/Django and based on The Expanse series, halfway between sci-fi harbormaster monitoring software and narrative tracking for fans."
 						],
 						[
 								"Into Infinity",
-								"../media/img/IntoInfinity.png",
+								"media/img/IntoInfinity.png",
 								"http://intoinfinity.herokuapp.com/",
 								"Also built with Python/Django as a mob-coding project, this space-exploration and teaching app is sized to the solar system, but scalable beyond, and tracks missions and objects in space"
 						],
 						[
 								"Give Your All",
-								"../media/img/GiveYourAll.png",
+								"media/img/GiveYourAll.png",
 								"http://giveyourall.herokuapp.com/",
 								"This app searches an API for charity data and totals up orders for donation amounts; such a design could conceivably handle/anonymize donations through Stripe or similar software"
 						]
@@ -67,23 +69,26 @@ export default function (){
 				}
 
     return(
-								<div id="Coding" class="tabContent">
-												<h3>Coding</h3>
-												<p>Here are a sampling of the various types of projects I have developed, ranging from purely front-end to full MERN-stack.</p>
-												<div id="pList">
-																<div id="buttons-title">        
-																				<button class="arrow" id="previous" onclick="clickPrev()">◄ Prev</button>
-																				<h3 id="pTitle"></h3>
-																				<button class="arrow" id="next" onclick="clickNext()">Next ►</button>
+        <>
+            <Navbar/>
+												<div id="Coding" class="tabContent">
+																<h3>Coding</h3>
+																<p>Here are a sampling of the various types of projects I have developed, ranging from purely front-end to full MERN-stack.</p>
+																<div id="pList">
+																				<div id="buttons-title">        
+																								<button class="arrow" id="previous" onclick="clickPrev()">◄ Prev</button>
+																								<h3 id="pTitle"></h3>
+																								<button class="arrow" id="next" onclick="clickNext()">Next ►</button>
+																				</div>
+																				<div id="pDetail">
+																								<a id="pLink" href="">
+																												<img id="pImg" src=""/>
+																												<p id="pDesc"></p>  
+																								</a>			                       
+																				</div>
 																</div>
-																<div id="pDetail">
-																				<a id="pLink" href="">
-																								<img id="pImg" src=""/>
-																								<p id="pDesc"></p>  
-																				</a>			                       
-																</div>
-												</div>
-								</div>    
+												</div>    
+         </>
     )
 
 }
---
+++ src/pages/Voice.jsx
@@ -0,0 +1,65 @@
+import "App" from '../App.css';
+import "Navbar" from '../components/Navbar.jsx';
+import "media" from '../media';
+
+export default function Voice(){
+
+				const voSamples = [
+								["media/av/bunker.mp4", "Stock a Bunker!"],
+								["media/av/balance.mp4", "Balance a Bike Wheel!"],
+								["media/av/worms.mp4", "Buzz Worms for Bait!"],
+								["media/av/tightrope.mp4", "Ride On a Tightrope!"],
+								["media/av/sardines.mp4", "Survive with Sardines!"],
+								["media/av/tracheotomy.mp4", "DIY Tracheotomy!"],
+								["media/av/mountain.mp4", "Scare Off a Mountain Lion!"],
+								["media/av/bear.mp4", "Survive a Bear Attack!"],
+								["media/av/croc.mp4", "What a Croc!"],
+								["media/av/tuna.mp4", "Gaff a Tuna!"]
+				];
+				
+				let i = 0;
+				let vidTitle = document.getElementById("vidTitle");
+				vidTitle.innerText = voSamples[i][1];
+				let vidLink = document.getElementById("voArray");
+				vidLink.setAttribute("src", voSamples[i][0]);
+				
+				function clickDown() {
+								i--;
+								if (i < 0) {
+										i = voSamples.length - 1;
+								}
+								vidTitle.innerText = voSamples[i][1];
+								vidLink.setAttribute("src", voSamples[i][0]);
+				}
+				function clickUp() {
+								i++;
+								if (i > voSamples.length - 1) {
+										i = 0;
+								}
+								vidTitle.innerText = voSamples[i][1];
+								vidLink.setAttribute("src", voSamples[i][0]);
+				}
+
+				return(
+        <>
+            <Navbar/>
+												<div id="Voice" class="tabContent">
+																<h3>Voice</h3>
+																<p>Some years ago, at the suggestions of colleagues, I studied a few courses with <a href="http://www.voiceone.com">Voice One</a> and lent my vocal training and talents to a series of marketing promos for several of Weldon Owen's books.</p>
+																<p>Production values were somewhere between "modest" and "DIY", but the marketing team managed to make it work, and it was a good way to demonstrate some range and show off a few titles I'd taken part in creating.</p>
+																<div id="voList">
+																				<div id="buttons-title">        
+																				<button class="arrow" id="previous" onclick="clickDown()">◄ Prev</button>
+																				<h4 id="vidTitle"></h4>
+																				<button class="arrow" id="next" onclick="clickUp()">Next ►</button>
+																</div>
+																<span id="vidPlay">
+																				<video id="voArray" src="" height="480" width="640" controls="controls">												
+																								Your browser does not support HTML video.
+																				</video>
+																</span>   
+												</div>
+        </>
+				)
+
+}
