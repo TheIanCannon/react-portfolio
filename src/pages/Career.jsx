0ba098b1b63f65174d447056605c81ebad0b48ef
Ian Cannon <ian.cannon@protonmail.com>
2022-03-02 13:18:52 -0800

    Swap to light mode; work with creating dark-mode icons later

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -22,7 +22,7 @@ export default function Career(){
 																								<p className="TechSkills"><img className="techSkill" alt="" src={django48}/> <img className="techSkill" alt="" src={mongodb48}/> <img className="techSkill" alt="" src={react48}/> <img className="techSkill" alt="" src={aws48}/> <img className="techSkill" alt="" src={node48}/> </p>																								
 																				</p>
 																    <p className="professionalWriter">As a professional writer and editor, I have had the honor of working with titles licensed from companies including Disney/Marvel, HBO, ViacomCBS, Ubisoft, and many more.
-																    <p>My past experience runs the gamut from customer service, to education as a university-level tutor, to EMS work including EMT-FTO and Firefighting certification.</p>
+																    
 																    </p>
 																    <p>
 																    <a href="mailto: ian.cannon@protonmail.com">Resume</a>&nbsp;available upon request,	or visit my <a href="https://www.linkedin.com/in/theiancannon/">LinkedIn</a> page for further details.</p>
