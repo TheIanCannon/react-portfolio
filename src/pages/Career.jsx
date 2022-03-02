2f80a24b6732f1ded442fab838c68d8213cd9a6a
Ian Cannon <ian.cannon@protonmail.com>
2022-03-02 11:45:10 -0800

    Render programming language and other technology icons in CAREER info

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -10,9 +10,6 @@ import node48 from '../media/img/node48.png';
 import python48 from '../media/img/python48.png';
 import react48 from '../media/img/react48.png';
 
-
-
-
 export default function Career(){
 
     return(
@@ -21,10 +18,10 @@ export default function Career(){
 												<div id="Career" className="tabContent">
 																<h3>Career</h3>
 																				<p className="myTraining">My training in full-stack software engineering with General Assembly's immersive program has given me familiarity with a range of coding and software development technologies.<br/>
-																								<p><b>Languages:</b> <html48.png/> <javascript48.png/> <css48.png/> <python48.png/> <git48.png/> </p>
-																								<p><b>Frameworks:</b> <django48.png/> </p>
-																								<p><b>Libraries:</b> <mongodb48.png/> <react48.png/> </p>
-																								<p><b>Platforms:</b> <aws48.png/> <node48.png/> </p>
+																								<p><img src={html48}/> <img src={javascript48}/> <img src={css48}/> <img src={python48}/> <img src={git48}/> </p>
+																								<p><b>Frameworks:</b> <img src={django48}/> </p>
+																								<p><b>Libraries:</b> <img src={mongodb48}/> <img src={react48}/> </p>
+																								<p><b>Platforms:</b> <img src={aws48}/> <img src={node48}/> </p>
 																				</p>
 																    <p className="professionalWriter">As a professional writer and editor, I have had the honor of working with titles licensed from companies including Disney/Marvel, HBO, ViacomCBS, Ubisoft, and many more.
 																    <p>My past experience runs the gamut from customer service, to education as a university-level tutor, to EMS work including EMT-FTO and Firefighting certification.</p>
