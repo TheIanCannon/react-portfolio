e3a926ddc917aefbc953777ee28b5133dfd92488
Ian Cannon <ian.cannon@protonmail.com>
2022-03-07 16:32:50 -0800

    Implement react carousel for Coding and Voice, hard-code details per carousel selection, N.B. is there an alternate way to do this without hard coding???

--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -1,4 +1,7 @@
 import Navbar from '../components/Navbar.jsx';
+
+import Carousel from 'react-elastic-carousel';
+
 import GiveYourAll from '../media/img/GiveYourAll.png';
 import OneArmedBandit from '../media/img/OneArmedBandit.png';
 import Palanthas from '../media/img/Palanthas.png';
@@ -10,68 +13,35 @@ export default function Coding(){
 				const projectList = [
 						[
 								"One-Armed Bandit",
-								{OneArmedBandit},
+								OneArmedBandit,
 								"http://theiancannon.github.io/GA-Project1-SlotMachine/",
 								"An HTML/CSS/JS-based slot machine with animated *.gif reels and contextual sound effects"
 						],
 						[
 								"Palanthas",
-								{Palanthas},
+								Palanthas,
 								"http://project-palanthas.herokuapp.com/",
 								"Developed in MongoDB/Express/Node for collecting, reviewing, and rating books"
 						],
 						[
 								"Ship Collector",
-								{ShipCollector},
+								ShipCollector,
 								"http://shipcollector.herokuapp.com/",
 								"Created using Python/Django and based on The Expanse series, halfway between sci-fi harbormaster monitoring software and narrative tracking for fans."
 						],
 						[
 								"Into Infinity",
-								{IntoInfinity},
+								IntoInfinity,
 								"http://intoinfinity.herokuapp.com/",
 								"Also built with Python/Django as a mob-coding project, this space-exploration and teaching app is sized to the solar system, but scalable beyond, and tracks missions and objects in space"
 						],
 						[
 								"Give Your All",
-								{GiveYourAll},
+								GiveYourAll,
 								"http://giveyourall.herokuapp.com/",
 								"This app searches an API for charity data and totals up orders for donation amounts; such a design could conceivably handle/anonymize donations through Stripe or similar software"
 						]
 				];
-
-				let p = 0;
-				let pTitle = document.getElementById("pTitle");
-				pTitle.innerText = projectList[p][0];
-				let pImg = document.getElementById("pImg");
-				pImg.src = projectList[p][1];
-				let pLink = document.getElementById("pLink");
-				pLink.href = projectList[p][2];
-				let pDesc = document.getElementById("pDesc");
-				pDesc.innerText = projectList[p][3];
-
-				function clickPrev(e) {
-        e.preventDefault();
-								p--;
-								if (p < 0) {
-										p = projectList.length - 1;
-								}
-								pTitle.innerText = projectList[p][0];
-								pImg.src = projectList[p][1];
-								pLink.href = projectList[p][2];
-								pDesc.innerText = projectList[p][3];
-				}
-				function clickNext(e) {
-        e.preventDefault();
-								p++;
-								if (p > projectList.length - 1) {
-										p = 0;
-								}
-								pTitle.innerText = projectList[p][0];
-								pImg.src = projectList[p][1];
-								pLink.href = projectList[p][2];
-								pDesc.innerText = projectList[p][3];
-				}
 
     return(
         <>
@@ -79,19 +49,14 @@ export default function Coding(){
 												<div id="Coding" className="tabContent">
 																<h3>Coding</h3>
 																<p>Here are a sampling of the various types of projects I have developed, ranging from purely front-end to full MERN-stack.</p>
-																<div id="pList">
-																				<div id="buttons-title">        
-																								<button className="arrow" id="previous" onclick={ e => clickPrev(e)}>◄ Prev</button>
-																								<p id="pTitle"></p>
-																								<button className="arrow" id="next" onclick={e => clickNext(e)}>Next ►</button>
-																				</div>
-																				<div id="pDetail">
-																								<a id="pLink" href="http://theiancannon.github.io/GA-Project1-SlotMachine/">
-																												<img alt="OneArmedBandit" id="pImg" src="{OneArmedBandit}"/>
-																												<p id="pDesc"></p>  
-																								</a>			                       
-																				</div>
-																</div>
+																<Carousel>
+																		      <p className="codeProject">{projectList[0][0]}{projectList[1][0]}<a href={projectList[2][0]}>{projectList[3][0]}</a></p>
+																		      <p className="codeProject">{projectList[0][1]}{projectList[1][1]}<a href={projectList[2][1]}>{projectList[3][1]}</a></p>
+																		      <p className="codeProject">{projectList[0][2]}{projectList[1][2]}<a href={projectList[2][2]}>{projectList[3][2]}</a></p>
+																		      <p className="codeProject">{projectList[0][3]}{projectList[1][3]}<a href={projectList[2][3]}>{projectList[3][3]}</a></p>
+																		      <p className="codeProject">{projectList[0][4]}{projectList[1][4]}<a href={projectList[2][4]}>{projectList[3][4]}</a></p>
+																		      <p className="codeProject">{projectList[0][5]}{projectList[1][5]}<a href={projectList[2][5]}>{projectList[3][5]}</a></p>
+                </Carousel>
 												</div>    
          </>
     )
--- src/pages/Voice.jsx
+++ src/pages/Voice.jsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar.jsx';
 import VideoPlayer from 'react-video-js-player';
+import Carousel from 'react-elastic-carousel';
 
 import bunker from '../media/av/bunker.mp4';
 import balance from '../media/av/balance.mp4';
@@ -15,59 +16,38 @@ import tuna from '../media/av/tuna.mp4';
 export default function Voice(){
 
 				const voSamples = [
-								[{bunker}, "Stock a Bunker!"],
-								[{balance}, "Balance a Bike Wheel!"],
-								[{worms}, "Buzz Worms for Bait!"],
-								[{tightrope}, "Ride On a Tightrope!"],
-								[{sardines}, "Survive with Sardines!"],
-								[{tracheotomy}, "DIY Tracheotomy!"],
-								[{mountain}, "Scare Off a Mountain Lion!"],
-								[{bear}, "Survive a Bear Attack!"],
-								[{croc}, "What a Croc!"],
-								[{tuna}, "Gaff a Tuna!"]
+								[bunker, "Stock a Bunker!"],
+								[balance, "Balance a Bike Wheel!"],
+								[worms, "Buzz Worms for Bait!"],
+								[tightrope, "Ride On a Tightrope!"],
+								[sardines, "Survive with Sardines!"],
+								[tracheotomy, "DIY Tracheotomy!"],
+								[mountain, "Scare Off a Mountain Lion!"],
+								[bear, "Survive a Bear Attack!"],
+								[croc, "What a Croc!"],
+								[tuna, "Gaff a Tuna!"],
 				];
-				
-				let i = 0;
-				let vidFile = voSamples[i][0];
-				let vidTitle = voSamples[i][1];
-				
-				// function clickDown(e) {
-    //     e.preventDefault();
-				// 				i--;
-				// 				if (i < 0) {
-				// 						i = voSamples.length - 1;
-				// 				}
-				// 				vidTitle.innerText = voSamples[i][1];
-				// 				vidLink.innerHTML("src", voSamples[i][0]);
-				// }
-				// function clickUp(e) {
-    //     e.preventDefault();
-				// 				i++;
-				// 				if (i > voSamples.length - 1) {
-				// 						i = 0;
-				// 				}
-				// 				vidTitle.innerText = voSamples[i][1];
-				// 				vidLink.innerHTML("src", voSamples[i][0]);
-				// }
-
+						
 				return(
         <>
             <Navbar/>
 												<div id="Voice" className="tabContent">
 																<h3>Voice</h3>
-																<p>Some years ago, at the suggestions of colleagues, I studied a few courses with <a href="http://www.voiceone.com">Voice One</a> and lent my vocal training and talents to a series of marketing promos for several of Weldon Owen's books.</p>
-																<p>Production values were somewhere between "modest" and "DIY", but the marketing team managed to make it work, and it was a good way to demonstrate some range and show off a few titles I'd taken part in creating.</p>
+																<p>I've always enjoyed reading for others, and multiple times I've been told that I have a good voice for it. Some years ago, at the suggestions of colleagues, I studied a few courses with <a href="http://www.voiceone.com">Voice One</a> and lent my vocal training and talents to a series of marketing promos for several of Weldon Owen's books.</p>
+																<p>Production values were somewhere between "modest" and "DIY", but Gonzalo Ferreyra and the marketing team at Weldon Owen managed to make it work, and it was a good way to demonstrate some range and show off a few titles I'd taken part in creating.</p>
 																<div id="voList">
-																				{/* <div id="buttons-title">        
-																								<button className="arrow" id="previous" onclick={e => clickDown(e)}>◄ Prev</button> */}
-																								&nbsp;&nbsp;<p id="vidTitle">{vidTitle}</p>&nbsp;&nbsp;
-																								{/* <button className="arrow" id="next" onclick={e => clickUp(e)}>Next ►</button>
-																    </div> */}
-																<span id="vidPlay">
-																				<VideoPlayer
-                    src={vidFile}
-                    />
-																</span>   
+																		  <Carousel>
+                        <p className="voSample"><VideoPlayer src={voSamples[0][0]}/>{voSamples[0][1]}</p>
+                        <p className="voSample"><VideoPlayer src={voSamples[1][0]}/>{voSamples[1][1]}</p>
+                        <p className="voSample"><VideoPlayer src={voSamples[2][0]}/>{voSamples[2][1]}</p>
+                        <p className="voSample"><VideoPlayer src={voSamples[3][0]}/>{voSamples[3][1]}</p>
+                        <p className="voSample"><VideoPlayer src={voSamples[4][0]}/>{voSamples[4][1]}</p>
+                        <p className="voSample"><VideoPlayer src={voSamples[5][0]}/>{voSamples[5][1]}</p>
+                        <p className="voSample"><VideoPlayer src={voSamples[6][0]}/>{voSamples[6][1]}</p>
+                        <p className="voSample"><VideoPlayer src={voSamples[7][0]}/>{voSamples[7][1]}</p>
+                        <p className="voSample"><VideoPlayer src={voSamples[8][0]}/>{voSamples[8][1]}</p>
+                        <p className="voSample"><VideoPlayer src={voSamples[9][0]}/>{voSamples[9][1]}</p>
+                    </Carousel>  
 												    </div>
             </div>
         </>
