5b6cedd6238334d4acdb28dbce80ec7adbb384d2
Ian Cannon <ian.cannon@protonmail.com>
2022-03-08 15:25:56 -0800

    Add note on VO page about React carousel

--- src/pages/Voice.jsx
+++ src/pages/Voice.jsx
@@ -10,6 +10,7 @@ export default function Voice(){
 																<h3>Voice</h3>
 																<p>I've always enjoyed reading for others, and multiple times I've been told that I have a good voice for it. Some years ago, at the suggestions of colleagues, I studied a few courses with <a href="http://www.voiceone.com">Voice One</a> and lent my vocal training and talents to a series of marketing promos for several of Weldon Owen's books.</p>
 																<p>Production values were somewhere between "modest" and "DIY", but Gonzalo Ferreyra and the marketing team at Weldon Owen managed to make it work, and it was a good way to demonstrate some range and show off a few titles I'd taken part in creating.</p>
+                <p>In addition, this set of voiceovers not only gave me experience working directly with sales and marketing, but allowed me to create the carousel that displays them here in ReactJS!</p>
 																<div id="voList">
 																		  <VideoCarousel/>
                         {/* <p className="voSample"><VideoPlayer src={voSamples[0][0]}/>{voSamples[0][1]}</p>
