16ff9c0e16397248b354de41c3d0b34cfafa8c64
Ian Cannon <ian.cannon@protonmail.com>
2022-03-08 15:02:48 -0800

    Continue refactoring video carousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,4 +1,8 @@
 import './VideoCarousel.css';
+import React from 'react';
+import {Carousel} from 'react-bootstrap';
+import ReactPlayer from 'react-player';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function VideoCarousel() {
 
