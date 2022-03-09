5218fe95c651abb67ea0295987a8a66c523ba55e
Ian Cannon <ian.cannon@protonmail.com>
2022-03-09 15:10:41 -0800

    Uninstall elastic carousel, import coding project links from coding page to new ProjectCarousel.jsx component

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -19,61 +19,61 @@ export default function VideoCarousel() {
 
     const voSamples = [
 								{
-										id: 1,
+										id: 0,
 										title: "Stock a Bunker!",
 										src: bunker,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
-										id: 2,
+										id: 1,
 										title: "Balance a Bike Wheel!",
 										src: balance,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
-										id: 3,
+										id: 2,
 										title: "Buzz Worms for Bait!",
 										src: worms,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
-										id: 4,
+										id: 3,
 										title: "Ride On a Tightrope!",
 										src: tightrope,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
-										id: 5,
+										id: 4,
 										title: "Survive with Sardines!",
 										src: sardines,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
-										id: 6,
+										id: 5,
 										title: "DIY Tracheotomy!",
 										src: tracheotomy,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
-										id: 7,
+										id: 6,
 										title: "Scare Off a Mountain Lion!",
 										src: mountain,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
-										id: 8,
+										id: 7,
 										title: "Survive a Bear Attack!",
 										src: bear,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
-										id: 9,
+										id: 8,
 										title: "What a Croc!",
 										src: croc,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
-										id: 10,
+										id: 9,
 										title: "Gaff a Tuna!",
 										src: tuna,
 										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -1,56 +1,8 @@
 import Navbar from '../components/Navbar.jsx';
 
-import Carousel from 'react-elastic-carousel';
-
-import GiveYourAll from '../media/img/GiveYourAll.png';
-import OneArmedBandit from '../media/img/OneArmedBandit.png';
-import Palanthas from '../media/img/Palanthas.png';
-import IntoInfinity from '../media/img/IntoInfinity.png';
-import ShipCollector from '../media/img/ShipCollector.png';
-import MoneyPal from '../media/img/MoneyPal.png';
-
 export default function Coding(){
 
-				const projectList = [
-						[
-								"One-Armed Bandit",
-								OneArmedBandit,
-								"http://theiancannon.github.io/GA-Project1-SlotMachine/",
-								"An HTML/CSS/JS-based slot machine with animated *.gif reels and contextual sound effects"
-						],
-						[
-								"Palanthas",
-								Palanthas,
-								"http://project-palanthas.herokuapp.com/",
-								"Developed in MongoDB/Express/Node for collecting, reviewing, and rating books"
-						],
-						[
-								"Ship Collector",
-								ShipCollector,
-								"http://shipcollector.herokuapp.com/",
-								"Created using Python/Django and based on The Expanse series, halfway between sci-fi harbormaster monitoring software and narrative tracking for fans."
-						],
-						[
-								"Into Infinity",
-								IntoInfinity,
-								"http://intoinfinity.herokuapp.com/",
-								"Also built with Python/Django as a mob-coding project, this space-exploration and teaching app is sized to the solar system, but scalable beyond, and tracks missions and objects in space"
-						],
-						[
-								"Give Your All",
-								GiveYourAll,
-								"http://giveyourall.herokuapp.com/",
-								"This app searches an API for charity data and totals up orders for donation amounts; such a design could conceivably handle/anonymize donations through Stripe or similar software"
-						],
-						[
-								"Money Pal",
-								MoneyPal,
-								"https://pensive-goodall-ea0925.netlify.app/",
-								"Developed in a February 2022 Hackathon with a team of UX/UI designers and fellow engineers, this simple financial analysis tool offers advice on a user's finances—and preserves anonymity by front-end-only design"
-						]
-				];
-
-    return(
+				return(
         <>
             <Navbar/>
 												<div id="Coding" className="tabContent">
