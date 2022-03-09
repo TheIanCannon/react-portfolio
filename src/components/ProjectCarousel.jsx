ed35107aaa035d045cd68ada862ffef79a871ba7
Ian Cannon <ian.cannon@protonmail.com>
2022-03-09 15:35:47 -0800

    Finish carousel setup with img, desc, and url, begin CSS

--- src/components/ProjectCarousel.jsx
+++ src/components/ProjectCarousel.jsx
@@ -12,44 +12,58 @@ import MoneyPal from '../media/img/MoneyPal.png';
 
 export default function ProjectCarousel(){
 
-const projectList = [
-						{
-								id: 0
-        "One-Armed Bandit",
-								OneArmedBandit,
-								"http://theiancannon.github.io/GA-Project1-SlotMachine/",
-								"An HTML/CSS/JS-based slot machine with animated *.gif reels and contextual sound effects"
-						},
-						{
-								"Palanthas",
-								Palanthas,
-								"http://project-palanthas.herokuapp.com/",
-								"Developed in MongoDB/Express/Node for collecting, reviewing, and rating books"
-						},
-						{
-								"Ship Collector",
-								ShipCollector,
-								"http://shipcollector.herokuapp.com/",
-								"Created using Python/Django and based on The Expanse series, halfway between sci-fi harbormaster monitoring software and narrative tracking for fans."
-						},
-						{
-								"Into Infinity",
-								IntoInfinity,
-								"http://intoinfinity.herokuapp.com/",
-								"Also built with Python/Django as a mob-coding project, this space-exploration and teaching app is sized to the solar system, but scalable beyond, and tracks missions and objects in space"
-						},
-						{
-								"Give Your All",
-								GiveYourAll,
-								"http://giveyourall.herokuapp.com/",
-								"This app searches an API for charity data and totals up orders for donation amounts; such a design could conceivably handle/anonymize donations through Stripe or similar software"
-						},
-						{
-								"Money Pal",
-								MoneyPal,
-								"https://pensive-goodall-ea0925.netlify.app/",
-								"Developed in a February 2022 Hackathon with a team of UX/UI designers and fellow engineers, this simple financial analysis tool offers advice on a user's finances—and preserves anonymity by front-end-only design"
-						}
-				];
+				const projectList = [
+								{
+										id: 0,
+										src: OneArmedBandit,
+										url: "http://theiancannon.github.io/GA-Project1-SlotMachine/",
+										desc: "One-Armed Bandit is an HTML/CSS/JS-based slot machine with animated *.gif reels and contextual sound effects."
+								},
+								{
+										id: 1,
+										src: Palanthas,
+										url: "http://project-palanthas.herokuapp.com/",
+										desc: "Palanthas was developed in MongoDB/Express/Node for collecting, reviewing, and rating books."
+								},
+								{
+										id: 2,
+										src: ShipCollector,
+										url: "http://shipcollector.herokuapp.com/",
+										desc: "ShipCollector, created using Python/Django as a fan project based on The Expanse sci-fi series, is halfway between sci-fi harbormaster monitoring software and narrative tracking."
+								},
+								{
+										id: 3,
+										src: IntoInfinity,
+										url: "http://intoinfinity.herokuapp.com/",
+										desc: "Into Infinity was also built with Python/Django as a mob-coding project. This space-exploration and teaching app is sized to the solar system, but scalable beyond, and tracks missions and objects in space."
+								},
+								{
+										id: 4,
+										src: GiveYourAll,
+										url: "http://giveyourall.herokuapp.com/",
+										desc: "Developed out of a personal interest in charity, Give Your All searches an API for charity data and totals up orders for donation amounts; such a design could conceivably handle/anonymize donations through Stripe or similar software"
+								},
+								{
+										id: 5,
+										src: MoneyPal,
+										url: "https://pensive-goodall-ea0925.netlify.app/",
+										desc: "Developed in a February 2022 Hackathon with a team of UX/UI designers and fellow engineers, Money Pal offers advice on a user's finances—and preserves anonymity by way of a front-end-only design."
+								}
+						];
+
+    return <div className="ProjectCarousel">
+								<Carousel>
+												{projectList.map((p) => {
+																return (
+																				<Carousel.Item key={p.id}>
+																							 <img className="projectImg" src={p.src} alt="{p.src}"/>
+																								<Carousel.Caption>
+																												<h6><a href={p.url}>{p.desc}</a></h6>                            
+																								</Carousel.Caption>
+																				</Carousel.Item>
+																)
+												})}
+        </Carousel>
+    </div>;
 
 }
--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -1,4 +1,5 @@
 import Navbar from '../components/Navbar.jsx';
+import ProjectCarousel from '../components/ProjectCarousel.jsx';
 
 export default function Coding(){
 
@@ -8,14 +9,7 @@ export default function Coding(){
 												<div id="Coding" className="tabContent">
 																<h3>Coding</h3>
 																<p>Here are a sampling of the various types of projects I have developed, ranging from purely front-end to full MERN-stack.</p>
-																<Carousel>
-																		      <p className="codeProject">{projectList[0][0]}<img alt="" src={projectList[0][1]}/><a href={projectList[0][2]}>{projectList[0][3]}</a></p>
-																		      <p className="codeProject">{projectList[1][0]}<img alt="" src={projectList[1][1]}/><a href={projectList[1][2]}>{projectList[1][3]}</a></p>
-																		      <p className="codeProject">{projectList[2][0]}<img alt="" src={projectList[2][1]}/><a href={projectList[2][2]}>{projectList[2][3]}</a></p>
-																		      <p className="codeProject">{projectList[3][0]}<img alt="" src={projectList[3][1]}/><a href={projectList[3][2]}>{projectList[3][3]}</a></p>
-																		      <p className="codeProject">{projectList[4][0]}<img alt="" src={projectList[4][1]}/><a href={projectList[4][2]}>{projectList[4][3]}</a></p>
-																		      <p className="codeProject">{projectList[5][0]}<img alt="" src={projectList[5][1]}/><a href={projectList[5][2]}>{projectList[5][3]}</a></p>
-                </Carousel>
+                <ProjectCarousel/>
 												</div>    
          </>
     )
