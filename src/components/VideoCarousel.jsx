6480f1849eb08afaff4dff595fb3062fad80dcad
Ian Cannon <ian.cannon@protonmail.com>
2022-03-15 13:20:47 -0700

    Move captions outside video box in voiceover carousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -17,66 +17,67 @@ import tuna from '../media/av/tuna.mp4';
 
 export default function VideoCarousel() {
 
+
     const voSamples = [
 								{
 										id: 0,
 										title: "Stock a Bunker!",
 										src: bunker,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
 										id: 1,
 										title: "Balance a Bike Wheel!",
 										src: balance,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
 										id: 2,
 										title: "Buzz Worms for Bait!",
 										src: worms,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
 										id: 3,
 										title: "Ride On a Tightrope!",
 										src: tightrope,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
 										id: 4,
 										title: "Survive with Sardines!",
 										src: sardines,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
 										id: 5,
 										title: "DIY Tracheotomy!",
 										src: tracheotomy,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
 										id: 6,
 										title: "Scare Off a Mountain Lion!",
 										src: mountain,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
 										id: 7,
 										title: "Survive a Bear Attack!",
 										src: bear,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
 										id: 8,
 										title: "What a Croc!",
 										src: croc,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								},
 								{
 										id: 9,
 										title: "Gaff a Tuna!",
 										src: tuna,
-										credit: "I. Cannon/G. Ferreyra/Weldon Owen"
+										credit: "2012 I. Cannon/G. Ferreyra/Weldon Owen"
 								}
 				];
 
@@ -87,14 +88,11 @@ export default function VideoCarousel() {
 																				<Carousel.Item key={vo.id}>
 																								<ReactPlayer
 																												url={vo.src}
-																												pip={true}
 																												controls={true}
 																												playing={false}
-																								/> 
-																								<Carousel.Caption>
-																												<h6>{vo.title}</h6>
-																												<h6>{vo.credit}</h6>
-																								</Carousel.Caption>
+																								/>
+                    				<h6 className="voInfo">{vo.title}</h6>
+																												<h6 className="voInfo">{vo.credit}</h6>    																								
 																				</Carousel.Item>
 																)
 												})}
